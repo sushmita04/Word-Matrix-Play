e58c312ddefc6030c1a4f222f821819c612a2d53
sushmita04 <57042819+sushmita04@users.noreply.github.com>
2020-06-16 00:31:07 +0530

    Update check-for-WR.cpp

--- check-for-WR.cpp
+++ check-for-WR.cpp
@@ -1,25 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string str;			//configuration
+
+void bin(unsigned n, int edges) { 
+	char temp[edges];
+	itoa(n, temp, 2);		//finds the unique configuration
+	str=temp;
+	if(str.size()!=edges) {
+		int a=str.size();
+		for(int i=0; i<edges; ++i) {
+			if(i<edges-a)
+				str[i]='0';
+			else
+				str[i]=temp[edges-a-i];
+			
+		}
+	}
+}
+
+int find_digraph() {
+	
+	
+}
+
 int main() {
 
-  vector <pair <int, int>> vect;
-	int n, i, j;
-	cin>> n;
-	int arr[n][n], clone[n][n]={0};
+ 	vector <pair <int, int>> vect;
+	int n, i, j, edges=0;
+	cin>> n;				//order of the matrix; number of vertices
+	int adj[n][n], Clone[n][n]={0};
 
 	//input of adjacency matrix
 	for(i=0; i<n; ++i) {
 		for(j=0; j<n; ++j) {
-			cin>>arr[i][j];
+			cin>>adj[i][j];
 		}
 	}
 	
+	
 	for(i=0; i<n; ++i) {
 		for(j=i+1; j<n; ++j) {
-			if(arr[i][j]==1) {
+			if(adj[i][j]==1) {
 				vect.push_back(make_pair (i, j));
-
+				++edges;	
 			}
 		}
 	}
