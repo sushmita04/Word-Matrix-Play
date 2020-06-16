9c5ef8d0f8f0d80cdd5ef0cbac6d91e7bc6983f9
sushmita04 <57042819+sushmita04@users.noreply.github.com>
2020-06-16 13:13:42 +0530

    Made functions for every task

--- check-for-WR.cpp
+++ check-for-WR.cpp
@@ -2,10 +2,51 @@
 using namespace std;
 
 string str;			//configuration
+int n, i, j, edges;
+vector <pair <int, int>> vect;
 
-int find_all_digraph() {
+int find_edges() {
+	edges=0;
+	for(i=0; i<n; ++i) {
+		for(j=i+1; j<n; ++j) {
+			if(adj[i][j]==1) {
+				vect.push_back(make_pair (i, j));
+				++edges;	
+			}
+		}
+	}
+	return edges;
+}
+
+bool find_all_digraph() {
+	//finds all the (2^edges) digraph configurations/ adjacency matrices 
+	for(i=0; i<pow(2,edges); ++i) {
+		bin(i, edges);				//gets the orientation
+		diag[n][n]={0};				//initialise the digraph to 0 before marking the directed edges
+		for(j=0; j<str.size(); ++j) {		//edges=str.size()
+			if(str[j]=='0')
+				diag[vect[j].first][vect[j].second]=1;
+			else if(str[j]=='1')
+				diag[vect[j].second][vect[j].first]=1;
+			
+		}
+		//find the path- DFS algorithm
+		bool flag=DFS();
+		
+		if(flag==1) {
+			cout<<"Word-representable";
+			return 1;
+		}
+		else
+			continue;
+	}
+	
+}
+
+bool DFS() {
 	
 	
+	//return 1 or 0 to indicate if word-representable or not respectively
 }
 
 void bin(unsigned n, int edges) { 
@@ -25,9 +66,6 @@ void bin(unsigned n, int edges) {
 }
 
 int main() {
-
- 	vector <pair <int, int>> vect;
-	int n, i, j, edges=0;
 	cin>> n;				//order of the matrix; number of vertices
 	int adj[n][n], diag[n][n]={0};
 
@@ -37,31 +75,5 @@ int main() {
 			cin>>adj[i][j];
 		}
 	}
-	
-	
-	for(i=0; i<n; ++i) {
-		for(j=i+1; j<n; ++j) {
-			if(adj[i][j]==1) {
-				vect.push_back(make_pair (i, j));
-				++edges;	
-			}
-		}
-	}
-	
-	//finds all the (2^edges) digraph configurations/ adjacency matrices 
-	for(i=0; i<pow(2,edges); ++i) {
-		bin(i, edges);				//gets the orientation
-		diag[n][n]={0};				//initialise the digraph to 0 before marking the directed edges
-		for(j=0; j<str.size(); ++j) {		//edges=str.size()
-			if(str[j]=='0')
-				diag[vect[j].first][vect[j].second]=1;
-			else if(str[j]=='1')
-				diag[vect[j].second][vect[j].first]=1;
-			
-		}
-		
-	}
-	
-	
 
 }
