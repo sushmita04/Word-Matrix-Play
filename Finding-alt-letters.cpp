18cd332c1d791f5f286bfcd0efd3ad8ee5e5f56c
sushmita04 <57042819+sushmita04@users.noreply.github.com>
2020-07-12 11:30:56 +0530

    Update Finding-alt-letters.cpp

--- Finding-alt-letters.cpp
+++ Finding-alt-letters.cpp
@@ -1,58 +1,86 @@
+/* Given a word, the program lists all the pairs of alternating letters in the context of word-representable graphs. */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	
+
 	int k, m, n;
 	bool flag;
-	string S,R="";
+	string S, Word = "";
 	cin>>S;
 
 	unordered_set<char>H;
 
-	for(auto ch:S){
-        	if(H.find(ch)==H.end()){
-        		R+=ch;
+	cout << "\nEnter the word: ";
+	for(auto ch:S)
+	{
+        	if(H.find(ch)==H.end())
+		{
+        		Word += ch;
         		H.insert(ch);
 		}
 	}
 
-	int size=R.length();
+	int size = Word.length();
 	vector<vector<int> > vec(size, vector<int> (size, 0));		//adjacency matrix
-	
-	for(int i=0;i<R.length();i++){
-        	for(int j=i+1;j<R.length();j++){
-    			k=0; m=0; n=1; flag=1;
-			while(S[k]!='\0') {            
-	    			if(S[k]==R[i] && n==1)
-	        		{   m=1; n=0;       }
-	        		else if(S[k]==R[j] && m==1)
-	        		{   n=1; m=0;       }
-	        		else if(S[k]==R[i] || S[k]==R[j])
-	        		{   flag=0; break;  }	            
+
+	cout << "\nThe alternating letters are:\n";
+	for(int i=0; i<Word.length(); i++)
+	{
+        	for(int j=i+1; j<Word.length(); j++)
+		{
+    			k = 0;
+			m = 0;
+			n = 1;
+			flag = 1;
+			while(S[k]!='\0') {
+	    			if(S[k]==Word[i] && n==1)
+	        		{
+					m = 1;
+				 	n = 0;
+				}
+	        		else if(S[k]==Word[j] && m==1)
+	        		{
+					n = 1;
+					m = 0;
+				}
+	        		else if(S[k]==Word[i] || S[k]==Word[j])
+	        		{
+					flag = 0;
+					break;
+				}
 	        		++k;
             		}
-	    		if(flag==1) {
-	    			cout<<R[i]<<"--"<<R[j]<<endl;
-	        		vec[i][j]=1;
-	        		vec[j][i]=1;   
+	    		if(flag==1)
+			{
+	    			cout << Word[i] << "--" << Word[j] << endl;
+	        		vec[i][j] = 1;
+	        		vec[j][i] = 1;
 	    		}
         	}
 	}
-	cout<<"\nThe adjacency matrix is:\n";
-	for (int i = -1; i < size; i++) {
-		if(i==-1) {
+	
+	cout << "\nThe adjacency matrix is:\n";
+	for(int i = -1; i < size; i++)
+	{
+		if(i==-1)
+		{
 			cout<<"  ";
-			for(char c:R)
-				cout<<c<<" ";
-			cout<<"\n";
+			for(char c:Word)
+			{
+				cout << c << " ";
+			}
+			cout << endl;
 		}
-		else {
-			cout<<R[i]<<" ";
-			for (int j = 0; j < size; j++) { 
-				cout<< vec[i][j]<< " "; 
-			} 
-			cout<< "\n"; 
+		else
+		{
+			cout << Word[i] << " ";
+			for(int j = 0; j < size; j++)
+			{
+				cout << vec[i][j] << " ";
+			}
+			cout << endl;
 		}
 	}
 	return 0;
